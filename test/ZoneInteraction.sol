9fea6ad75c979845ba318e033c6a45f8d5c900ce
Alec DiFederico <alecdifederico@gmail.com>
2023-12-11 16:30:01 -0500

    added basic test

---
+++ test/ZoneInteraction.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Test} from "@forge-std/Test.sol";
+
+import {ZoneInteraction} from "src/lib/ZoneInteraction.sol";
+
+contract Accumulator_Unit_Test is Test, Accumulator {}
+    function test_Success_AssertRestrictedAdvancedOrderValidity() public {}
